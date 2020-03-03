eb8555ef13a63d30237678b63049ca8b462fee3c
Josh Blumberg <jlblumberg@gmail.com>
2020-03-03 18:30:08 +0000

    Commit WIP step six

--- note-controller.js
+++ note-controller.js
@@ -1,2 +1,22 @@
-var myDoc = document.getElementById("app")
-myDoc.innerHTML = "Hello Bob.";
+var NoteController = function(aNoteList) {
+  this.noteList = aNoteList
+  this.noteList.newNote("Favourite drink: seltzer");
+  this.noteListView = new NoteListView(this.noteList)
+}
+
+NoteController.prototype = (function() {
+
+  function getHtmlAndInsertInApp() {
+    var myDoc = document.getElementById("app")
+    myDoc.innerHTML = this.noteListView.returnHtml()
+  } 
+ 
+  return {
+    getHtmlAndInsertInApp
+  }
+
+})();
+
+// noteList = new NoteList();
+// controller = new NoteController(noteList);
+// controller.getHtmlAndInsertInApp();
